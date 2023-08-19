3d6e4514d0a7448f89c31e6512cc66340abd7e87
Sachin-Mhetre <118711683+Sachin-Mhetre@users.noreply.github.com>
2023-08-19 16:11:32 +0530

    Create Optimized_bubble_sort .c

---
+++ Sachin DSL/Sorting/Optimized_bubble_sort .c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+
+void optimizedBubbleSort(int arr[], int size) {
+    int swapped;
+
+    for (int i = 0; i < size - 1; i++) {
+        swapped = 0; // Initialize flag to track whether any swaps occurred
+
+        for (int j = 0; j < size - i - 1; j++) {
+            if (arr[j] > arr[j + 1]) {
+                // Swap arr[j] and arr[j + 1]
+                int temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+
+                swapped = 1; // Set flag to indicate a swap occurred
+            }
+        }
+
+        // If no two elements were swapped in the inner loop, the array is already sorted
+        if (swapped == 0) {
+            break;
+        }
+    }
+}
+
+int main() {
+    int arr[] = {64, 34, 25, 12, 22, 11, 90};
+    int size = sizeof(arr) / sizeof(arr[0]);
+
+    printf("Original array: ");
+    for (int i = 0; i < size; i++) {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+
+    optimizedBubbleSort(arr, size);
+
+    printf("Sorted array: ");
+    for (int i = 0; i < size; i++) {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+
+    return 0;
+}
