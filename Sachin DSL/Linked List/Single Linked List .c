8e8a37a092ace38b246428567c518213445e26af
Sachin-Mhetre <118711683+Sachin-Mhetre@users.noreply.github.com>
2023-08-28 10:37:44 +0530

    Update Single Linked List .c

--- Sachin DSL/Linked List/Single Linked List .c
+++ Sachin DSL/Linked List/Single Linked List .c
@@ -55,6 +55,44 @@ struct node * afternode(struct node * head , struct node * prevnode ,int data){
     return head;
 };
 
+struct node * deleteatbegin(struct node * head){
+   struct node * p = head ;
+   head = head->next;
+};
+
+struct node * deletenode(struct node * head,int key){
+    struct node * temp = head, *prev;
+    while(temp != NULL && temp->data !=key){
+        prev = temp ;
+        temp = temp->next;
+    }
+    if(temp==NULL) {
+        return head;
+    }
+    prev->next = temp->next;
+};
+
+struct node * deleteatend(struct node * head){
+    struct node * ptr = head;
+    while(ptr->next->next != NULL){
+        ptr = ptr->next;
+    }
+    ptr->next=NULL;
+};
+
+struct node * search(struct node * head,int key){
+    struct node * ptr = head;
+    while(ptr!=NULL){
+        if(ptr->data==key){
+        printf("Found\n");
+        return head;
+    }
+        ptr = ptr -> next;
+    }
+      printf("not found\n");
+      return head;
+};
+
 int main() {
     int ops=0;
     struct node * head;
@@ -77,19 +115,33 @@ int main() {
     printf("Linked list is : \n");
     linkedlisttraversal(head);
     printf("Select the correct option : \n");
-    printf("1) Insert at Begin \n2) Insert at index \n3) Insert at end \n4) Insert after node \n");
+    printf("1) Insert at Begin \n2) Insert at index \n3) Insert at end \n4) Insert after node \n5) Delete at begin \n6) Delete node \n7)Delete at end\n8) Search\n");
     scanf("%d",&ops);
     switch(ops){
         case 1:
-           head = insertatbegin(head,78);
+            head = insertatbegin(head,78);
            break;
         case 2:
-           head = insertatindex(head,87,1);
-           break;
+            head = insertatindex(head,87,1);
+            break;
         case 3:
-           head = insertatend(head,89);
+            head = insertatend(head,89);
+            break;
         case 4:
-           head = afternode(head,third,44);
+            head = afternode(head,third,44);
+            break;
+        case 5:
+        	head = deleteatbegin(head);
+        	break;
+        case 6:
+            head = deletenode(head,47);
+            break;
+        case 7:
+            head = deleteatend(head);
+            break;
+        case 8:
+            head = search(head,88);
+		     
     }
     linkedlisttraversal(head);
     return 0;
