b61909fbdba959f75d3ca8f2b00f2d1614e92400
Sachin-Mhetre <118711683+Sachin-Mhetre@users.noreply.github.com>
2023-08-29 11:49:22 +0530

    Delete sachin PPL directory

--- sachin PPL/Factorial.py
+++
@@ -1,9 +0,0 @@
-def fac(n):
-    f = 1
-    for i in range(1,n+1):
-        f = f * i
-    return f
-        
-a = int(input("Enter the number : ")) 
-result = fac(a)
-print("Factorial is " , result)
