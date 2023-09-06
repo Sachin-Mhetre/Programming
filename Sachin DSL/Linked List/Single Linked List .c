9847b83202adec3dab1611de1f229f09748aeacc
Sachin-Mhetre <118711683+Sachin-Mhetre@users.noreply.github.com>
2023-09-06 18:40:52 +0530

    Update Single Linked List .c

--- Sachin DSL/Linked List/Single Linked List .c
+++ Sachin DSL/Linked List/Single Linked List .c
@@ -3,18 +3,19 @@
 #include<stdlib.h>
 
 struct node{
-    int data;
-    struct node * next;
+  int data;
+  struct node * next;
 };
 
-void linkedlisttraversal(struct node * ptr){
+void traversal(struct node * head){
+    struct node * ptr = head;
     while(ptr!=NULL){
         printf("Element : %d\n",ptr->data);
-        ptr = ptr ->next;
+        ptr = ptr->next;
     }
 }
 
-struct node * insertatbegin(struct node * head,int data){
+struct node * insertatbegin(struct node * head , int data){
     struct node * ptr = (struct node *)malloc(sizeof(struct node));
     ptr -> data = data;
     ptr -> next = head;
@@ -22,127 +23,151 @@ struct node * insertatbegin(struct node * head,int data){
 };
 
 struct node * insertatindex(struct node * head , int data , int index){
-    struct node * ptr = (struct node*)malloc(sizeof(struct node));
+    int i =0;
+    struct node * ptr = (struct node *)malloc(sizeof(struct node));
     struct node * p = head;
-    int i=0;
     while(i!=index-1){
         p = p->next;
         i++;
     }
     ptr->data = data;
-    ptr->next = p->next;
-    p->next = ptr;
+    ptr->next = p -> next;
+    p -> next = ptr;
     return head;
 };
 
-struct node * insertatend(struct node * head , int data){
-    struct node * ptr = (struct node *)malloc(sizeof(struct node));
+struct node * insertatend(struct node * head,int data){
+   struct node * ptr = (struct node * )malloc(sizeof(struct node));
+   struct node * p = head;
+   while(p->next!=NULL){
+       p = p->next;
+   }
+   ptr -> data = data;
+   ptr -> next = NULL;
+   p->next = ptr;
+   return head;
+};
+
+struct node * deleteatbegin(struct node * head ){
     struct node * p = head;
-    ptr->data = data;
-    while(p->next!=NULL){
-        p = p->next;
-    }
-    p->next = ptr;
-    ptr->next = NULL;
+    head = head->next;
+    free(p);
     return head;
 };
 
-struct node * afternode(struct node * head , struct node * prevnode ,int data){
-    struct node * ptr = (struct node *)malloc(sizeof(struct node));
-    ptr->data = data;
-    ptr->next = prevnode->next;
-    prevnode->next =ptr;
+struct node * deleteatindex(struct node * head , int index){
+    struct node * p = head;
+    struct node * q = head->next;
+    for(int i = 0 ; i < index - 1;i++){
+        p = p -> next; 
+        q = q-> next;
+    }
+    p->next = q->next;
+    free(q);
     return head;
-};
-
-struct node * deleteatbegin(struct node * head){
-   struct node * p = head ;
-   head = head->next;
-};
-
-struct node * deletenode(struct node * head,int key){
-    struct node * temp = head, *prev;
-    while(temp != NULL && temp->data !=key){
-        prev = temp ;
-        temp = temp->next;
-    }
-    if(temp==NULL) {
-        return head;
-    }
-    prev->next = temp->next;
 };
 
 struct node * deleteatend(struct node * head){
-    struct node * ptr = head;
-    while(ptr->next->next != NULL){
-        ptr = ptr->next;
+    struct node * p = head;
+    struct node * q = head->next;
+    while(q->next!=NULL){
+        p = p->next;
+        q = q->next;
     }
-    ptr->next=NULL;
+    p->next = q->next;
+    free(q);
+    return head;
 };
 
-struct node * search(struct node * head,int key){
-    struct node * ptr = head;
-    while(ptr!=NULL){
-        if(ptr->data==key){
-        printf("Found\n");
-        return head;
+struct node * search(struct node * head , int key){
+  struct node * ptr = head ;
+  int i =0;
+  while(ptr!=NULL){
+      if(ptr->data == key){
+          printf("Key found\n");
+          i++;
+          return head;
+      }
+      ptr = ptr -> next;
+  }
+  printf("not found \n ");
+  return head;
+};
+
+struct node * sort(struct node * head){
+    int temp;
+    struct node * p = head;
+    while(p->next!=NULL){
+        struct node * q = p->next;
+        while(q!=NULL){
+            if(p->data > q->data){
+                temp = p ->data;
+                p -> data = q -> data;
+                q -> data = temp;
+            }
+            q = q->next;
+        }
+        p = p->next;
     }
-        ptr = ptr -> next;
-    }
-      printf("not found\n");
-      return head;
+    return head;
 };
 
 int main() {
-    int ops=0;
+    int ops;
     struct node * head;
     struct node * second;
     struct node * third;
+    head = (struct node *)malloc(sizeof(struct node));
+    second = (struct node *)malloc(sizeof(struct node));
+    third = (struct node *)malloc(sizeof(struct node));
     
-    head = (struct node * )malloc(sizeof(struct node));
-    second = (struct node * )malloc(sizeof(struct node));
-    third = (struct node * )malloc(sizeof(struct node));
-    
-    head -> data = 7;
+    head -> data = 106;
     head -> next = second;
     
     second -> data = 47;
     second -> next = third;
     
-    third -> data = 77;
+    third -> data = 98;
     third -> next = NULL;
-    
-    printf("Linked list is : \n");
-    linkedlisttraversal(head);
-    printf("Select the correct option : \n");
-    printf("1) Insert at Begin \n2) Insert at index \n3) Insert at end \n4) Insert after node \n5) Delete at begin \n6) Delete node \n7)Delete at end\n8) Search\n");
+    printf("1) Insert at begin \n2) Insert at index \n3) Insert at end \n4) Delete at Begin \n5) Delete at index \n6) Delete at end \n7) Search \n8) Sort \n9) Exit \n");
     scanf("%d",&ops);
     switch(ops){
-        case 1:
-            head = insertatbegin(head,78);
-           break;
-        case 2:
-            head = insertatindex(head,87,1);
-            break;
-        case 3:
-            head = insertatend(head,89);
-            break;
-        case 4:
-            head = afternode(head,third,44);
-            break;
-        case 5:
-        	head = deleteatbegin(head);
-        	break;
-        case 6:
-            head = deletenode(head,47);
-            break;
-        case 7:
-            head = deleteatend(head);
-            break;
-        case 8:
-            head = search(head,88);
-		     
+            case 1:
+               head = insertatbegin(head,7);
+               break;
+               
+            case 2:
+               head = insertatindex(head,68,1);
+               break;
+               
+            case 3:
+               head = insertatend(head,98);
+               break;
+               
+            case 4:
+               head = deleteatbegin(head);
+               break;
+               
+            case 5 :
+               head = deleteatindex(head,1);
+               break;
+               
+            case 6 : 
+               head = deleteatend(head);
+               break;
+               
+            case 7 :
+               head = search(head,7);
+               break;
+               
+            case 8 :
+               head = sort(head);
+               break;
+               
+            case 9:
+               break;
     }
-    linkedlisttraversal(head);
+
+    traversal(head);
     return 0;
 }
