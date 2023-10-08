0d4637d651e6788ae2be3faa4f6b73460c4afe98
Sachin-Mhetre <118711683+Sachin-Mhetre@users.noreply.github.com>
2023-10-08 15:29:03 +0530

    Update Queue.c

--- Sachin DSL/Self/Queue.c
+++ Sachin DSL/Self/Queue.c
@@ -1,4 +1,3 @@
-// Online C compiler to run C program online
 #include <stdio.h>
 #define N 5
 
@@ -6,54 +5,50 @@ int queue[N];
 int front = -1;
 int rear = -1;
 
-void enqueue(int x){
-    if(rear = N-1){
+void enqueue(int x) {
+    if (rear == N - 1) {
         printf("Overflow\n");
-    }
-    else if(front==-1 && rear == -1){
-        front=rear=0;
+    } else if (front == -1 && rear == -1) {
+        front = rear = 0;
         queue[rear] = x;
-    }
-    else{
+    } else {
         rear++;
         queue[rear] = x;
     }
 }
 
-void dequeue(){
-    if(front==-1 && rear == -1){
-        printf("underflow\n");
-    }
-    else if(front==rear){
-        front=rear=-1;
-    }
-    else{
-        printf("The Dequeued element is : %d\n",queue[front]);
+void dequeue() {
+    if (front == -1 && rear == -1) {
+        printf("Underflow\n");
+    } else if (front == rear) {
+        printf("The Dequeued element is : %d\n", queue[front]);
+        front = rear = -1;
+    } else {
+        printf("The Dequeued element is : %d\n", queue[front]);
         front++;
     }
 }
 
-void display(){
-    int i ;
-    if(front == -1 && rear ==-1){
-        printf("Queue is empty");
-    }
-    else{
-        for(i=front;i<rear+1;i++){
-            printf("Element : %d\n",queue[i]);
+void display() {
+    int i;
+    if (front == -1 && rear == -1) {
+        printf("Queue is empty\n");
+    } else {
+        for (i = front; i <= rear; i++) {
+            printf("Element : %d\n", queue[i]);
         }
     }
 }
 
-void peek(){
-    if(front==-1 && rear==-1){
+void peek() {
+    if (front == -1 && rear == -1) {
         printf("Empty Queue\n");
-    }
-    else{
-        printf("%d\n",queue[front]);
+    } else {
+        printf("%d\n", queue[front]);
     }
 }
-void main() {
+
+int main() {
     enqueue(2);
     enqueue(5);
     enqueue(-1);
