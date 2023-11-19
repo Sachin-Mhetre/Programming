7613ca9f33aabbb416b3ec85473655b3eb987fbd
Sachin-Mhetre <118711683+Sachin-Mhetre@users.noreply.github.com>
2023-11-19 18:25:37 +0530

    Update stack_using_sll.c

--- Sachin DSL/Stack/stack_using_sll.c
+++ Sachin DSL/Stack/stack_using_sll.c
@@ -1,58 +1,59 @@
-#include<stdio.h>
+// Online C compiler to run C program online
+#include <stdio.h>
 
 struct node{
-   int data;
-   struct node * link;
+    int data;
+    struct node * link;
 };
 
 struct node * top = NULL;
+
 void push(int data){
-    struct node * newnode;
-    newnode=(struct node *)malloc(sizeof(struct node));
-    newnode->data=data;
-    newnode->link=top;
+    struct node * newnode = (struct node*)malloc(sizeof(struct node));
+    newnode->data = data;
+    newnode->link = top;
     top = newnode;
 }
 
 void display(){
-    struct node *temp = top;
-    if(top == NULL){
-        printf("List is empty");
+    struct node * temp = top;
+    if(top==NULL){
+        printf("\nStack underflow");
     }
     else{
-        while (temp !=NULL){
-            printf("%d ", temp -> data);
-            temp =temp->link;
+        while(temp!=NULL){
+            printf("%d ",temp->data);
+            temp = temp->link;
         }
     }
 }
 
 void peek(){
     if(top==NULL){
-        printf("List is empty");
+        printf("\nStack underflow");
     }
     else{
-        printf("%d is the topmost element",top->data);
+        printf("\nTop element is : %d\n",top->data);
     }
 }
 
 void pop(){
     struct node * temp = top;
     if(top==NULL){
-        printf("List underflow");
+        printf("\nStack underflow");
     }
     else{
-        printf("%d is popped element",top->data);
+        printf("\npoped element is : %d\n",top->data);
         top = temp->link;
         free(temp);
     }
 }
 
-void main(){
+int main() {
      push(2);
      push(3);
-     puch(5);
-     displau();
+     push(5);
+     display();
      peek();
      pop();
      peek();
